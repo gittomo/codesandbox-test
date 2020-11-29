78448edc61ff12076d9cde35f83019758b557214
gittomo <48152904+gittomo@users.noreply.github.com>
2020-11-29 12:39:02 +0900

    1129_2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,26 @@ const onClickAdd = () => {
 
   const div = document.createElement("div");
   // div.className = "list-row";
+
+  div.className = "list-row";
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  div.appendChild(li);
+  document.getElementById("incomplete-list").appendChild(div);
   console.log(div);
+
+  // const completeButton = document.createElement("button");
+  // completeButton.innerText = "完了";
+
+  // const deleteButton = document.createElement("button");
+  // deleteButton.innerText = "削除";
+
+  // div.appendChild(li);
+
+  // div.appendChild(completeButton);
+  // div.appendChild(deleteButton);
 };
 
 document
